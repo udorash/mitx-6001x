12eacdd0532e926e18e66ae9f7001baa45040d15
Uladzislau Dorash <udorash@gmail.com>
2015-02-06 03:00:50 +0300

    Pass grader tests for problem set 4

--- ProblemSet4/ps4b.py
+++ ProblemSet4/ps4b.py
@@ -1,7 +1,6 @@
 from ps4a import *
 import time
 
-
 #
 #
 # Problem #6: Computer chooses a word
@@ -9,7 +8,7 @@ import time
 #
 def compChooseWord(hand, wordList, n):
     """
-    Given a hand and a wordList, find the word that gives 
+    Given a hand and a wordList, find the word that gives
     the maximum value score, and return it.
 
     This word should be calculated by considering all the words
@@ -23,25 +22,17 @@ def compChooseWord(hand, wordList, n):
 
     returns: string or None
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
-    # Create a new variable to store the maximum score seen so far (initially 0)
+    bestScore = 0
+    bestWord = None
 
-    # Create a new variable to store the best word seen so far (initially None)  
+    for word in wordList:
+        if isValidWord(word, hand, wordList):
+            score = getWordScore(word, n)
+            if score > bestScore:
+                bestScore = score
+                bestWord = word
 
-    # For each word in the wordList
-
-        # If you can construct the word from your hand
-        # (hint: you can use isValidWord, or - since you don't really need to test if the word is in the wordList - you can make a similar function that omits that test)
-
-            # Find out how much making that word is worth
-
-            # If the score for that word is higher than your best score
-
-                # Update your best score, and best word accordingly
-
-
-    # return the best word you found.
-
+    return bestWord
 
 #
 # Problem #7: Computer plays a hand
@@ -49,24 +40,41 @@ def compChooseWord(hand, wordList, n):
 def compPlayHand(hand, wordList, n):
     """
     Allows the computer to play the given hand, following the same procedure
-    as playHand, except instead of the user choosing a word, the computer 
+    as playHand, except instead of the user choosing a word, the computer
     chooses it.
 
     1) The hand is displayed.
     2) The computer chooses a word.
-    3) After every valid word: the word and the score for that word is 
-    displayed, the remaining letters in the hand are displayed, and the 
+    3) After every valid word: the word and the score for that word is
+    displayed, the remaining letters in the hand are displayed, and the
     computer chooses another word.
     4)  The sum of the word scores is displayed when the hand finishes.
     5)  The hand finishes when the computer has exhausted its possible
     choices (i.e. compChooseWord returns None).
- 
+
     hand: dictionary (string -> int)
     wordList: list (string)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    
+    totalScore = 0
+
+    while sum(hand.values()) > 0:
+        print "Current Hand: ",
+        displayHand(hand)
+        word = compChooseWord(hand, wordList, n)
+        if word == None:
+            break
+        else:
+            if isValidWord(word, hand, wordList):
+                score = getWordScore(word, n)
+                totalScore += score
+                print "\"{0}\" earned {1} points. Total {2} points\n".format(word, score, totalScore)
+                hand = updateHand(hand, word)
+            else:
+                print "Invalid word, please try again.\n"
+
+    print "Total score: {} points.".format(totalScore)
+
 #
 # Problem #8: Playing a game
 #
@@ -74,7 +82,7 @@ def compPlayHand(hand, wordList, n):
 def playGame(wordList):
     """
     Allow the user to play an arbitrary number of hands.
- 
+
     1) Asks the user to input 'n' or 'r' or 'e'.
         * If the user inputs 'e', immediately exit the game.
         * If the user inputs anything that's not 'n', 'r', or 'e', keep asking them again.
@@ -85,25 +93,48 @@ def playGame(wordList):
     3) Switch functionality based on the above choices:
         * If the user inputted 'n', play a new (random) hand.
         * Else, if the user inputted 'r', play the last hand again.
-      
+
         * If the user inputted 'u', let the user play the game
           with the selected hand, using playHand.
-        * If the user inputted 'c', let the computer play the 
+        * If the user inputted 'c', let the computer play the
           game with the selected hand, using compPlayHand.
 
     4) After the computer or user has played the hand, repeat from step 1
 
     wordList: list (string)
     """
-    # TO DO... <-- Remove this comment when you code this function
-    print "playGame not yet implemented." # <-- Remove this when you code this function
+    hand = {}
+    cmdPrompt = "Enter n to deal a new hand, r to replay the last hand, or e to end game: "
+    cmd2Prompt = "Enter u to have yourself play, c to have the computer play: "
 
-        
+    while True:
+        cmd = raw_input(cmdPrompt)
+        while not cmd in ["n", "r", "e"]:
+            print "Invalid command."
+            cmd = raw_input(cmdPrompt)
+
+        if cmd == "e":
+            return
+        elif cmd == "n":
+            hand = dealHand(HAND_SIZE)
+        elif cmd == "r":
+            if len(hand) == 0:
+                print "You have not played a hand yet. Please play a new hand first!\n"
+                continue
+
+        cmd2 = raw_input(cmd2Prompt)
+        while not cmd2 in ["u", "c"]:
+            print "Invalid command."
+            cmd2 = raw_input(cmd2Prompt)
+
+        if cmd2 == "u":
+            playHand(hand, wordList, HAND_SIZE)
+        elif cmd2 == "c":
+            compPlayHand(hand, wordList, HAND_SIZE)
+
 #
 # Build data structures used for entire session and play game
 #
 if __name__ == '__main__':
     wordList = loadWords()
     playGame(wordList)
-
-
